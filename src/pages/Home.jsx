f2da07fe61dce633b20836ba3e7b052b4aae3d5d
WesamAbadi <wisamabady7@gmail.com>
2022-04-09 15:54:12 +0200

    Changed structure

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Random from "../components/Random";
+import Popular from "../components/Popular";
+
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <Popular />
+      <Random />
+    </div>
+  );
+}
+
+export default Home;
